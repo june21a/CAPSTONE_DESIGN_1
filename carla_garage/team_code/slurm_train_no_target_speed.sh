cdc497feef4e70fcfb22f1e404e8dffe138fbbb9
june21a <june21r@gmail.com>
2026-05-27 06:21:47 +0000

    training crash if --use_disk_cache is True

--- carla_garage/team_code/slurm_train_no_target_speed.sh
+++ carla_garage/team_code/slurm_train_no_target_speed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=tf_010_0
+#SBATCH --job-name=tf_wp_010_0
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=3-00:00
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=32
-#SBATCH --output=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/tfpp_010_0_%a_%A.out  # File to which STDOUT will be written
-#SBATCH --error=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/tfpp_010_0_%a_%A.err   # File to which STDERR will be written
+#SBATCH --output=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/tfpp_wp_010_0_%a_%A.out  # File to which STDOUT will be written
+#SBATCH --error=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/tfpp_wp_010_0_%a_%A.err   # File to which STDERR will be written
 #SBATCH --partition=a100-galvani
 
 # IMPORTANT: Start this script from within team_code folder, otherwise it will not work
@@ -22,8 +22,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/miniconda3/envs/garage_2/bin/python
 export OMP_NUM_THREADS=32  # Limits pytorch to spawn at most num cpus cores threads
 export OPENBLAS_NUM_THREADS=1  # Shuts off numpy multithreading, to avoid threads spawning other threads.
 torchrun --nnodes=1 --nproc_per_node=1 --max_restarts=0 --rdzv_id=$SLURM_JOB_ID --rdzv_backend=c10d \
-    train.py --id debug_train --use_disk_cache 0 --crop_image 1 --seed 0 --epochs 31 --batch_size 16 --lr 3e-4 --setting all \
+    train.py --id train_no_target_speed --use_disk_cache 0 --crop_image 1 --seed 0 --epochs 31 --batch_size 16 --lr 3e-4 --setting all \
     --root_dir /home/ec2-user/AD_challenge/CAPSTONE_DESIGN_1/carla_garage/training_data \
     --logdir /home/ec2-user/AD_challenge/experiments \
-    --use_controller_input_prediction 1 --continue_epoch 0 --cpu_cores 32 --num_repetitions 1 --use_cosine_schedule 1 --cosine_t0 1 \
+    --use_controller_input_prediction 0 --use_wp_gru 1 --continue_epoch 0 --cpu_cores 32 --num_repetitions 1 --use_cosine_schedule 1 --cosine_t0 1 \
     --image_architecture regnety_032 --lidar_architecture regnety_032
